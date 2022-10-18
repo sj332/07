05d12ee7af799d782847e80c6472d3f33274ec31
sj332 <112758738+sj332@users.noreply.github.com>
2022-10-18 16:53:04 +0900

    Add files via upload

--- main.c
+++ main.c
@@ -3,17 +3,22 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+void f(void);
+
+int i;
 int main(int argc, char *argv[]) {
-	
-	int i=10;
-	printf("함수 호출전 i=%d\n",i);
-	inc(i);
-	printf("함수 호출후 i=%d\n",i);
+	//전역변수, 범위는 소스파일 전체(연결경우x)
+	for (i=0;i<5;i++) //5번  
+	{
+		f(); //전역 변수 i가 같은 i라서 (data 영역의 i) 
+	}
 	return 0;
+}
 	
-}
-int inc(int counter)
-{
-	counter++;	
-	return counter;
-}
+	void f(void)
+	{
+		for(i=0;i<10;i++) //10번 
+		printf("#");
+	}
+
+
